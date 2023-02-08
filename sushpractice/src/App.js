33515a6a80ab5f0fe7283c019db304aa3cb5f48a
Sush25N <97217610+Sush25N@users.noreply.github.com>
2023-02-08 19:31:31 +0530

    final

--- sushpractice/src/App.js
+++ sushpractice/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
+import UseEffects from './Hooks/UseEffect/UseEffect';
 // import IncDec from "./Hooks/UseState/IncDec"
 import ChangedState from './Hooks/UseState/ChangedState' 
 import ChangeState from './Hooks/UseState/ChangedState'
+import UseEffects from './Hooks/UseEffect/UseEffect';
 
 function App() {
   return (
     <>
       {/* <IncDec /> */}
       <ChangedState /> <ChangeState />
+      <UseEffects />
     </>
   );
 }
